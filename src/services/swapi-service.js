24958d085f2e11a73574aa8bfc5adf05755ee27e
Roman Feditskiy <feditskiy@gmail.com>
2018-12-05 17:57:02 +0200

    Added error message to random planet component

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -13,28 +13,70 @@ export default class SwapiService {
 
   async getAllPeople() {
     const res = await this.getResource(`/people/`);
-    return res.results;
+    return res.results.map(this._transformPerson);
   }
 
-  getPerson(id) {
-    return this.getResource(`/people/${id}/`)
+  async getPerson(id) {
+    const res = await this.getResource(`/people/${id}/`)
+    return this._transformPerson(res);
   }
 
   async getAllPlanets() {
     const res = await this.getResource(`/planets/`);
-    return res.results;
+    return res.results.map(this._transformPlanet);
   }
 
-  getPlanet(id) {
-    return this.getResource(`/planet/${id}/`)
+  async getPlanet(id) {
+    const planet = await this.getResource(`/planets/${id}/`);
+    return this._transformPlanet(planet);
   }
 
   async getAllStarships() {
     const res = await this.getResource(`/starships/`);
-    return res.results;
+    return res.results.map(this._transformStarship);
   }
 
-  getStarship(id) {
-    return this.getResource(`/starship/${id}/`)
+  async getStarship(id) {
+    const res = await this.getResource(`/starships/${id}/`);
+    return this._transformStarship(res);
+  }
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet(planet) {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter
+    }
+  }
+
+  _transformPerson(person) {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birth_year,
+      eyeColor: person.eye_color
+    }
+  }
+
+  _transformStarship(starships) {
+    return {
+      id: this._extractId(starships),
+      name: starships.name,
+      model: starships.model,
+      manufacturer: starships.manufacturer,
+      costInCredits: starships.cost_in_credits,
+      length: starships.length,
+      crew: starships.crew,
+      passengers: starships.passengers,
+      cargoCapacity: starships.cargo_capacity
+    }
   }
 }
