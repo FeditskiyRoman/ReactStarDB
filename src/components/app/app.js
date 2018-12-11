3ec37f9f5b28abab937228ff80600cae4023c352
Roman Feditskiy <feditskiy@gmail.com>
2018-12-11 18:46:59 +0200

    Added router

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,6 +11,9 @@ import { SwapiServiceProvider } from '../swapi-service-context';
 
 import './app.css';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { StarshipDetails } from '../sw-components';
+
 export default class App extends Component {
 
   state = {
@@ -32,15 +35,29 @@ export default class App extends Component {
     return (
       <ErrorBoundry>
         <SwapiServiceProvider value={this.state.swapiService} >
-          <div className="stardb-app">
-            <Header onServiceChange={this.onServiceChange} />
 
-            <RandomPlanet />
-            <PeoplePage />
-            <PlanetsPage />
-            <StarshipsPage />
+          <Router>
+            <div className="stardb-app col-md-12">
+              <Header onServiceChange={this.onServiceChange} />
+              <RandomPlanet />
 
-          </div>
+              <Switch>
+                <Route path="/"
+                      render={() => <h2>Welcome</h2>}
+                      exact/>
+                <Route path="/people/:id?" component={PeoplePage}/>
+                <Route path="/planets" component={PlanetsPage}/>
+                <Route path="/starships" exact component={StarshipsPage}/>
+                <Route path="/starships/:id"
+                      render={({match}) => {
+                        const { id } = match.params;
+                        return <StarshipDetails itemId={id}/>
+                      }}/>
+                <Route render={() => <h2>Page not found</h2>}/>
+              </Switch>
+
+            </div>
+          </Router>
         </SwapiServiceProvider>
       </ErrorBoundry>
     );
